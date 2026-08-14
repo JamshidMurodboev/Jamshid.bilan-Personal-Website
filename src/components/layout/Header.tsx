da0a66240a060062360190ec27b7d4a1a07e6faf
JamshidMurodboev <jamshid.murodboev.edu@gmail.com>
2026-08-14 11:27:30 +0500

    fix: remove resources/booking from nav, rename News & Blog to News

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -212,9 +212,6 @@ function HeaderInner() {
     { href: `/${locale}/results`, label: t('results') },
     { href: `/${locale}/news`, label: t('news') },
     { href: `/${locale}/services`, label: t('services') },
-    { href: `/${locale}/resources`, label: t('resources') },
-    { href: `/${locale}/booking`, label: t('booking') },
-    { href: `/${locale}/contact`, label: t('contact') },
   ];
 
   return (
